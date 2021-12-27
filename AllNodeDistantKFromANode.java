521187d01721a6c5a683d5f526daa443a51bcb67
Saurabh Sharma <saurabhsvits@gmail.com>
2021-12-27 16:49:15 -0500

    Update AllNodeDistantKFromANode.java

--- AllNodeDistantKFromANode.java
+++ AllNodeDistantKFromANode.java
@@ -9,8 +9,69 @@
  */
 
 // LeetCode problem #863
-class Solution {    
 
+class Solution {     
+    // DFS.
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        List<Integer> result = new ArrayList<>();
+        if(root.left == null && root.right == null)
+        {
+            if(k == 0)
+            {
+                result.add(root.val);
+            }
+        }
+        else
+            dfs(root, target, k, 0, result);
+        return result;        
+    }
+    
+    public int dfs(TreeNode root, TreeNode target, int k, int dist, List<Integer> result)
+    {
+        if(root == null)
+            return -1;
+        if(root.val == target.val)
+        {
+            checkSubtree(root, result, 0, k);
+            return 1;
+        }
+        int left = dfs(root.left, target, k, dist, result);
+        int right = dfs(root.right, target, k, dist, result);
+        if(left != -1)
+        {
+            if(left == k)
+                result.add(root.val);
+            checkSubtree(root.right, result, left + 1, k);
+            return left + 1; // returning parent(or next in the path while traveling from target)'s distance from target. 
+        }
+        if(right != -1)
+        {
+            if(right == k)
+                result.add(root.val);
+            checkSubtree(root.left, result, right + 1, k);
+            return right + 1;
+        }
+        return -1;
+    }
+    
+    public void checkSubtree(TreeNode root, List<Integer> result, int dist, int k)
+    {
+        if(root == null)
+            return;
+        if(dist == k)
+        {
+            result.add(root.val);
+            return;
+        }
+        checkSubtree(root.left, result, dist + 1, k);
+        checkSubtree(root.right, result, dist + 1, k);
+    }
+    
+}
+
+    
+    // Approach #1. Create link to parent.
+    /*
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
        Map<Integer, TreeNode> map = new HashMap<>();              
        createLinkToParent(root, map);                
@@ -82,4 +143,4 @@ class Solution {
         createLinkToParent(root.left, map);
         createLinkToParent(root.right, map);
     }
-}
+*/
